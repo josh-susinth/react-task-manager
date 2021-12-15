3e1de95710be1874b41ff5da38c4e3729c46d98f
josh susinth <48217196+josh-susinth@users.noreply.github.com>
2021-12-16 00:00:45 +0530

    Add files via upload

---
+++ task-manager/src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = ({color,text,onclick}) => {
+    
+    return (
+        <button onClick={onclick} style={{backgroundColor:color}} className='btn'>{text}</button>
+    )
+}
+
+export default Button
---
+++ task-manager/src/components/Header.js
@@ -0,0 +1,18 @@
+
+const Header = (props) => {
+    
+    return (
+        <header className="header">
+            <h1>{props.title}</h1>
+            
+            
+        </header>
+    )
+}
+
+// const headingStyle={
+//     color:'red',
+//     backgroundColor:'black'
+//   }
+  
+export default Header
